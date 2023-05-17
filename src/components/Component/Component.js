400dbaa8770685636c072e81218a6b99614deb59
David Gluzman <david.m.gluzman@gmail.com>
2023-05-16 21:59:29 -0400

    add component files

--- src/components/Component/Component.js
+++ src/components/Component/Component.js
@@ -1,8 +1,22 @@
 import "./component.scss";
 import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
+import Heading from "../Heading/Heading";
+import underHeadingCounters from "./underHeadingCounters";
+import commentSection from "./commentSection";
+import nextVideo from "./nextVideo";
 
 function Component() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Heading />
+      <underHeadingCounters />
+      <commentSection />
+      <nextVideo />
+    </>
+  );
 }
 
-export default App;
+export default Component;
