cee87106ac5ec4fa3a4d0795997d3d4e1f285545
David Gluzman <david.m.gluzman@gmail.com>
2023-05-28 15:23:10 -0400

    finish sass elements for all screen sizes on the UploadPage

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,5 +1,5 @@
 import image from "../../assets/images/Upload-video-preview.jpg";
-import upload from "../../assets/icons/upload.svg";
+import publish from "../../assets/icons/publish.svg";
 import { useNavigate } from "react-router-dom";
 import "./UploadPage.scss";
 
@@ -16,39 +16,43 @@ function UploadPage() {
     <div className="upload-page">
       <h1>Upload Page</h1>
       <div className="content">
-        <div className="content__image-container">
-          <h4 className="content__image-container__heading">VIDEO THUMBNAIL</h4>
-          <img
-            src={image}
-            className="content__image-container__image"
-            alt="Bicycle-stear"
-          />
-        </div>
         <form onSubmit={alertSubmit}>
-          <div className="content__form">
-            <label className="content__form__label">
-              TITLE YOUR VIDEO
-              <input
-                name="titleYourVideo"
-                className="content__form__input"
-                placeholder="Add a title to your video"
+          <div className="content__div">
+            <div className="content__image-container">
+              <h4 className="content__image-container__heading">
+                VIDEO THUMBNAIL
+              </h4>
+              <img
+                src={image}
+                className="content__image-container__image"
+                alt="Bicycle-stear"
               />
-            </label>
-            <label className="content__form__label">
-              ADD A VIDEO DESCRIPTION
-              <input
-                name="describeYourVideo"
-                className="content__form__input content__form__input--modifier"
-                placeholder="Add a description to your video"
-              />
-            </label>
+            </div>
+            <div className="content__form">
+              <label className="content__form__label">
+                TITLE YOUR VIDEO
+                <input
+                  name="titleYourVideo"
+                  className="content__form__input"
+                  placeholder="Add a title to your video"
+                />
+              </label>
+              <label className="content__form__label">
+                ADD A VIDEO DESCRIPTION
+                <input
+                  name="describeYourVideo"
+                  className="content__form__input content__form__input--modifier"
+                  placeholder="Add a description to your video"
+                />
+              </label>
+            </div>
           </div>
           <div className="content__buttons">
             <button className="content__buttons__cancel-button">CANCEL</button>
             <button className="content__buttons__publish-button">
               <img
                 className="content__buttons__publish-button__icon"
-                src={upload}
+                src={publish}
                 alt="upload-icon"
               />
               <p className="content__buttons__publish-button__text">PUBLISH</p>
